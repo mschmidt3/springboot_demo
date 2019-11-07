83071645ba510fd49800b06c19e46ae781ef3967
msm <michael.schmidt@opitz-consulting.com>
2019-11-07 15:06:11 +0100

    select conroller method by http-method and return type

--- src/main/java/de/opitz/consulting/example/demo/controllers/HelloWorldController.java
+++ src/main/java/de/opitz/consulting/example/demo/controllers/HelloWorldController.java
@@ -1,13 +1,34 @@
 package de.opitz.consulting.example.demo.controllers;
 
+import javax.activation.MimeType;
+
+import org.slf4j.*;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
+    Logger log = LoggerFactory.getLogger(HelloWorldController.class);
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String index(){
+        log.info("index called");
         return "Hello From HelloWorldConroller!";
     }
+
+    @RequestMapping(value="/hello", method=RequestMethod.GET, produces="text/html")
+    public String getType(){
+        log.info("index called");
+        return "<html><body>\n"+
+               "<h1>Hello From HelloWorldConroller!"+"</h1>\n"+
+               "</body></html>";
+    }
+
+    @PostMapping("/hello")
+    public String postHello(){
+        log.info("postHello called");
+        return "Received Post";
+    }
+
 }
