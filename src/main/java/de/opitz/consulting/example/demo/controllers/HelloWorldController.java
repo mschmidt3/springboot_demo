f118004670a882f2eb810803437736ee452180c9
msm <michael.schmidt@opitz-consulting.com>
2019-11-08 12:13:33 +0100

    some more Mapping examples

--- src/main/java/de/opitz/consulting/example/demo/controllers/HelloWorldController.java
+++ src/main/java/de/opitz/consulting/example/demo/controllers/HelloWorldController.java
@@ -20,7 +20,9 @@ public class HelloWorldController {
     @RequestMapping(value="/hello", method=RequestMethod.GET, produces="text/html")
     public String getType(){
         log.info("index called");
-        return "<html><body>\n"+
+        return "<!doctype html>\n"+
+               "<html><head><title>hello</title></head>\n"+
+               "<body>\n"+
                "<h1>Hello From HelloWorldConroller!"+"</h1>\n"+
                "</body></html>";
     }
@@ -30,5 +32,27 @@ public class HelloWorldController {
         log.info("postHello called");
         return "Received Post";
     }
+    
+    @GetMapping("/hello/{name}")
+    public String getElement( @PathVariable String name){
+        log.info("getElement called {}", name);
+        return "Hello "+ name;
+    }
+
+    @GetMapping("/hello3/**")
+    public String getAnyPath(){
+        log.info("getAnyPath called ");
+        return "Hello 3 ";
+    }
+
+    @GetMapping("/hello2")
+    public String helloWithParam(
+        @RequestParam(value="name", defaultValue="World") String name,
+        @RequestHeader("accept-language") String language
+        ){
+        log.info("helloWithParam called langguage:{}", language);
+        return "Hello "+ name;
+    }
+
 
 }
