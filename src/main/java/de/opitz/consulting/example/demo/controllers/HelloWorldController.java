c24805d3076bfafe9e330aae4e0d1c98bf0decb2
msm <michael.schmidt@opitz-consulting.com>
2019-11-08 12:54:41 +0100

    Hello World contoller

--- src/main/java/de/opitz/consulting/example/demo/controllers/HelloWorldController.java
+++ src/main/java/de/opitz/consulting/example/demo/controllers/HelloWorldController.java
@@ -1,9 +1,9 @@
 package de.opitz.consulting.example.demo.controllers;
 
 import javax.activation.MimeType;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.*;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,37 +12,34 @@ public class HelloWorldController {
     Logger log = LoggerFactory.getLogger(HelloWorldController.class);
 
     @GetMapping("/hello")
-    public String index(){
+    public String index() {
         log.info("index called");
         return "Hello From HelloWorldConroller!";
     }
 
-    @RequestMapping(value="/hello", method=RequestMethod.GET, produces="text/html")
-    public String getType(){
+    @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = "text/html")
+    public String getType() {
         log.info("index called");
-        return "<!doctype html>\n"+
-               "<html><head><title>hello</title></head>\n"+
-               "<body>\n"+
-               "<h1>Hello From HelloWorldConroller!"+"</h1>\n"+
-               "</body></html>";
+        return "<!doctype html>\n" + "<html><head><title>hello</title></head>\n" + "<body>\n"
+                + "<h1>Hello From HelloWorldConroller!" + "</h1>\n" + "</body></html>";
     }
 
     @PostMapping("/hello")
-    public String postHello(){
+    public String postHello() {
         log.info("postHello called");
         return "Received Post";
     }
-    
+
     @GetMapping("/hello/{name}")
-    public String getElement( @PathVariable String name){
+    public String getElement(@PathVariable String name) {
         log.info("getElement called {}", name);
-        return "Hello "+ name;
+        return "Hello " + name;
     }
 
     @GetMapping("/hello3/**")
-    public String getAnyPath(){
-        log.info("getAnyPath called ");
-        return "Hello 3 ";
+    public String getAnyPath(HttpServletRequest request) {
+        log.info("getAnyPath called {}", request.getRequestURI() );
+        return "Hello 3 " + request.getRequestURI();
     }
 
     @GetMapping("/hello2")
