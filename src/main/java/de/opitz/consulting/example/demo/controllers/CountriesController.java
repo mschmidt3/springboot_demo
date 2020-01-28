616769801f206bd80829e88c6489874c4a36592a
msm <michael.schmidt@opitz-consulting.com>
2020-01-28 12:55:06 +0100

    Cross Origin erlauben

--- src/main/java/de/opitz/consulting/example/demo/controllers/CountriesController.java
+++ src/main/java/de/opitz/consulting/example/demo/controllers/CountriesController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,6 +27,7 @@ import de.opitz.consulting.example.demo.ElementNotFoundException;
 import de.opitz.consulting.example.demo.entities.Countries;
 import de.opitz.consulting.example.demo.repositories.CountriesRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/countries")
 class CountriesController {
