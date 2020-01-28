40b30ebf53a9de8dce08b3e4fd94054958a86777
msm <michael.schmidt@opitz-consulting.com>
2020-01-28 10:39:47 +0100

    Countries- create

--- src/main/java/de/opitz/consulting/example/demo/controllers/CountriesController.java
+++ src/main/java/de/opitz/consulting/example/demo/controllers/CountriesController.java
@@ -1,14 +1,19 @@
 package de.opitz.consulting.example.demo.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import de.opitz.consulting.example.demo.ElementNotFoundException;
 import de.opitz.consulting.example.demo.entities.Countries;
@@ -41,5 +46,15 @@ class CountriesController {
             throw new ElementNotFoundException("Country with code ["+code+"] not found.");
         }
         return element.get();
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Object> create(@RequestBody Countries element){
+        Countries savedElement = repository.save(element);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedElement.getId()).toUri();
+ 
+        return ResponseEntity.created(location).build();
     }
 }
