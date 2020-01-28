f6ae2278eb1bdfb6819d7017babbe96ae5d9e94c
msm <michael.schmidt@opitz-consulting.com>
2020-01-28 10:17:43 +0100

    ElementNotFoundException

--- src/main/java/de/opitz/consulting/example/demo/controllers/CountriesController.java
+++ src/main/java/de/opitz/consulting/example/demo/controllers/CountriesController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import de.opitz.consulting.example.demo.ElementNotFoundException;
 import de.opitz.consulting.example.demo.entities.Countries;
 import de.opitz.consulting.example.demo.repositories.CountriesRepository;
 
@@ -26,6 +27,9 @@ class CountriesController {
     @GetMapping("/{name}")
     public Countries retrive(@PathVariable String name) {
         Optional<Countries> element = repository.findByName(name);
+        if(!element.isPresent()){
+            throw new ElementNotFoundException("Country "+name+" not found.");
+        }
         return element.get();
     }
 }
