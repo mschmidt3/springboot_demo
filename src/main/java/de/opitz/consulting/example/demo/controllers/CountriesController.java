a6ed9d5ce4aad88977c782148599c692a4b643aa
msm <michael.schmidt@opitz-consulting.com>
2020-01-28 10:11:30 +0100

    Countries findByName ohne Fehlerprüfung

--- src/main/java/de/opitz/consulting/example/demo/controllers/CountriesController.java
+++ src/main/java/de/opitz/consulting/example/demo/controllers/CountriesController.java
@@ -1,25 +1,31 @@
 package de.opitz.consulting.example.demo.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.opitz.consulting.example.demo.entities.Countries;
 import de.opitz.consulting.example.demo.repositories.CountriesRepository;
 
-
-
 @RestController
 @RequestMapping("/api/countries")
-class CountriesController{
+class CountriesController {
     @Autowired
     CountriesRepository repository;
 
     @GetMapping("")
-    public List<Countries> index(){
+    public List<Countries> index() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{name}")
+    public Countries retrive(@PathVariable String name) {
+        Optional<Countries> element = repository.findByName(name);
+        return element.get();
     }
 }
--- src/main/java/de/opitz/consulting/example/demo/repositories/CountriesRepository.java
+++ src/main/java/de/opitz/consulting/example/demo/repositories/CountriesRepository.java
@@ -1,5 +1,7 @@
 package de.opitz.consulting.example.demo.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import de.opitz.consulting.example.demo.entities.Countries;
@@ -7,4 +9,5 @@ import de.opitz.consulting.example.demo.entities.Countries;
 @Repository
 public interface CountriesRepository extends JpaRepository<Countries, Long> {
 
+	Optional<Countries> findByName(String name);
 }
